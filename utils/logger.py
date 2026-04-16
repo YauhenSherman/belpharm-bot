fb1b1453aea5661583d9ac022302ee718061f794
Eugene <jenia.s9020@gmail.com>
2026-04-17 00:00:34 +0300

    chore: reduce noisy logs

--- utils/logger.py
+++ utils/logger.py
@@ -7,10 +7,11 @@ def setup_logger() -> logging.Logger:
         format="%(asctime)s | %(levelname)s | %(name)s | %(message)s",
     )
 
-    # Делаем шумные библиотеки тише
+    # Глушим лишний шум библиотек
     logging.getLogger("httpx").setLevel(logging.WARNING)
     logging.getLogger("httpcore").setLevel(logging.WARNING)
-    logging.getLogger("telegram.ext.ExtBot").setLevel(logging.WARNING)
+    logging.getLogger("telegram").setLevel(logging.INFO)
+    logging.getLogger("telegram.ext").setLevel(logging.INFO)
 
     logger = logging.getLogger("belpharm_bot")
     logger.setLevel(logging.INFO)
